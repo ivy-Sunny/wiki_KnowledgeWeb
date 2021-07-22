96d28101782cda31ee1f26adf37056d071847dd5
ivy-Sunny <thelife_dream@icloud.com>
2021-07-23 07:41:55 +0800

    集成Axios插件

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,39 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import Antd from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'ant-design-vue/dist/antd.css';
 
-createApp(App)
-    .use(store)
+import * as Icons from '@ant-design/icons-vue';
+import axios from "axios";
+
+
+const app = createApp(App);
+app.use(store)
     .use(router)
     .use(Antd)
-    .mount('#app')
+    .mount('#app');
+
+/*全局图标使用*/
+/*const icons: any = Icons;
+for (const iconsKey in icons) {
+    app.component(iconsKey, icons[iconsKey]);
+}*/
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER
+axios.interceptors.response.use(response => {
+    console.log("响应结果:", response);
+    return response.data;
+}, error => {
+    return Promise.reject(error);
+})
+axios.interceptors.request.use(request => {
+    console.log("请求信息:", request);
+    return request;
+}, error => {
+    return Promise.reject(error)
+})
+
+console.log('环境：' + process.env.NODE_ENV);
+console.log('服务端：' + process.env.VUE_APP_SERVER)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
+              <span>
+                <user-outlined/>
+                subnav 1111
+              </span>
+          </template>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="ebooks"
+              :grid="{gutter:20, column: 3}">
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
+              </template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
+import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
   },
+  setup() {
+    const ebooks = ref();
+
+    const actions: Record<string, string>[] = [
+      {type: 'StarOutlined', text: '156'},
+      {type: 'LikeOutlined', text: '156'},
+      {type: 'MessageOutlined', text: '2'},
+    ];
+    onMounted(() => {
+      axios.get("ebook/list").then(res => {
+        ebooks.value = res.data
+      });
+    })
+
+    return {
+      ebooks,
+      actions,
+    }
+  }
 });
 </script>
+<style scoped>
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 10%;
+  margin: 5px 0;
+}
+
+>>> .ant-layout ant-layout-has-sider {
+  height: calc(100vh - 63px - 70px);
+}
+</style>
